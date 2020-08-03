a9bbca05db2bcc5c483cafafbb0be058ff17833f
katebatrakova <kateryna.batrakova@gmail.com>
2020-08-03 18:38:46 -0400

    Update a DayList  to render DayListItems

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,16 +1,18 @@
 import React from "react";
 import DayListItem from "components/DayListItem";
 
-
+//The DayList is responsible for rendering a list of DayListItem components.
 export default function DayList(props) {
+
+  //days:Array a list of day objects (each object includes an id, name, and spots)
   const { days } = props
   const parsedDays = days.map((day) => {
     return (<DayListItem
 
       name={day.name}
       spots={day.spots}
-      selected={day.name === day.day}
-      setDay={day.setDay}
+      selected={day.name === props.day}
+      setDay={props.setDay}
     />
 
     );

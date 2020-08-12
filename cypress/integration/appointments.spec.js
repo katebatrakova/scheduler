9acc047ec03b151293693f57e60eac4e02e11f1c
katebatrakova <kateryna.batrakova@gmail.com>
2020-08-12 19:14:10 -0400

    Create Cypress tests for Add, Edit, Delete

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,77 @@
+describe("Appointments", () => {
+  //separate the common test commands 
+  beforeEach(() => {
+    //reset the db in the beginning
+    cy.request("GET", "http://localhost:8001/api/debug/reset")
+
+    //visit the root of the web server
+    cy.visit("/");
+    // confirm that the DOM contains the text "Monday"
+    cy.contains("Monday")
+  })
+  it("should book an interview", () => {
+    //select the add button
+    cy.get("[alt='Add']")
+      // clicks the first button for the empty appointment 
+      .first()
+      .click()
+    //find the input field
+    cy.get('[data-testid=student-name-input]')
+      //type the name 
+      .type('Lydia Miller-Jones')
+
+    //select the int interviewer 
+    cy.get("[alt='Sylvia Palmer']")
+      .click()
+    //click the save button.
+    cy.contains("Save")
+      .click()
+    //verify that we show the student and interviewer names 
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+
+  });
+
+
+  it("should edit an interview", () => {
+
+    //select the edit button
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+    // //find the input field
+    cy.get('[data-testid=student-name-input]')
+      //clear the input field
+      .clear()
+      //type the name
+      .type('Lydia Miller-Jones')
+    // //select the int interviewer 
+    cy.get("[alt='Tori Malcolm']")
+      .click()
+    //click the save button.
+    cy.contains("Save")
+      .click()
+    // //verify updates  student and interviewer names are shown
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+
+  });
+
+  it("should cancel an interview", () => {
+
+    //select the cancel button
+    cy.get("[alt=Delete]")
+      .click({ force: true });
+
+    cy.contains("Confirm")
+      .click()
+
+    cy.contains("Deleting")
+    cy.contains("Deleting").should('not.exist')
+
+    cy.contains(".appointment__card--show", "Archie Cohen").should('not.exist')
+
+
+  });
+
+});
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,9 @@
 describe("Navigation", () => {
-  it("should visit root", () => {
+  it("should navigate to Tuesday", () => {
     cy.visit("/");
+    cy.contains("li", "Tuesday")
+      .click()
+      .should('have.class', 'day-list__item--selected')
+
   });
 });
